ed72cab6cf3519d58ad536f2cdeb56e5164b8ec3
prem <premkattamanchi@gmail.com>
2023-02-20 10:41:36 +0530

    recent

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -20,7 +20,7 @@ public class ImageService {;
     @Autowired
     ImageRepository imageRepository2;
 
-    public Image addImage(Integer blogId, String description, String dimensions){
+    public void addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
         Blog blog=blogRepository2.findById(blogId).get();
         Image image=new Image(dimensions,description,blog);
@@ -31,7 +31,6 @@ public class ImageService {;
         blog.setImageList(list);
 
         blogRepository2.save(blog);
-        return image;
     }
 
     public void deleteImage(Integer id){
